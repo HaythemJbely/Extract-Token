eb98b661084a647fdb077bde317a8a69ff793584
Haythem.Djebali <Haythem.Djebali@cynapsys.local>
2021-12-16 14:55:45 +0100

    first commit

--- src/main/java/com/teck/spring/security/postgresql/payload/response/MessageResponse.java
+++ src/main/java/com/teck/spring/security/postgresql/payload/response/MessageResponse.java
@@ -3,8 +3,8 @@ package com.teck.spring.security.postgresql.payload.response;
 public class MessageResponse {
     private String message;
 
-    public  MessageResponse(String message) {
-    	System.out.println("vg");
+    public MessageResponse(String message) {
+        System.out.println("vg");
         this.message = message;
     }
 
